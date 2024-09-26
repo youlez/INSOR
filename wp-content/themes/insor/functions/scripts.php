3353f3da7eb784d54a33938979cb9de88169b52c
Youlez <karent.pinzon@idartes.gov.co>
2024-09-25 19:37:26 -0500

    presentacion 26 sept

--- wp-content/themes/insor/functions/scripts.php
+++ wp-content/themes/insor/functions/scripts.php
@@ -23,15 +23,19 @@ function cargar_estilos()
     wp_register_style('bootstrap', get_template_directory_uri() . '/node_modules/bootstrap/dist/css/bootstrap.css', array(), false, 'all');
     wp_register_style('style', get_template_directory_uri() . '/style.css', array(), '2024.09.19.01', 'all');
     wp_register_style('header', get_template_directory_uri() . '/css/header.css', array(), '2024.09.19.01', 'all');
+    wp_register_style('home', get_template_directory_uri() . '/css/home.css', array(), '2024.09.19.01', 'all');
 
     wp_enqueue_style('bootstrap');
     wp_enqueue_style('header');
     wp_enqueue_style('style');
-}
-function custom_tooltip_script() {
-    wp_enqueue_script( 'tooltip-script', get_template_directory_uri() . '/js/tooltip.js', array(), null, true );
+    wp_enqueue_style('home');
 }
 
-add_action( 'wp_enqueue_scripts', 'custom_tooltip_script' );
+function custom_tooltip_script()
+{
+    wp_enqueue_script('tooltip-script', get_template_directory_uri() . '/js/tooltip.js', array(), null, true);
+}
+
+add_action('wp_enqueue_scripts', 'custom_tooltip_script');
 add_action('wp_enqueue_scripts', 'cargar_scripts');
 add_action('wp_enqueue_scripts', 'cargar_estilos');
