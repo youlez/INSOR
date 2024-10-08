cd43013216b29576d06f517db18a47b5bf1f0632
Youlez <karent.pinzon@idartes.gov.co>
2024-10-08 11:39:49 -0500

    ajuste de footer

--- wp-content/themes/insor/footer.php
+++ wp-content/themes/insor/footer.php
@@ -3,91 +3,93 @@
 <footer>
   <div class="position-relative">
     <div class="gov-fondo position-absolute"></div>
-    <div class="govco-data-front position-relative container">
-      <div class="row py-3 p-sm-4">
-        <div class="col-12 col-lg-7">
-          <h4 class="m-0 f-7">Instituto Nacional Para Sordos</h4>
+    <div class="container">
+      <div class="govco-data-front position-relative">
+        <div class="row py-3 p-sm-4">
+          <div class="col-12 col-lg-7">
+            <h4 class="m-0 f-7">Instituto Nacional Para Sordos</h4>
 
-          <h5 class="mt-4 f-6">Sede Principal</h5>
-          <p>
-            <b>Dirección: </b> Carrera 89A No. 64C–30. Álamos Zona Industrial. Bogotá D.C. – Colombia.<br />
-            <b>Código Postal:</b> 111071<br />
-            <b>Horario de atención Presencial:</b> Lunes a Viernes 8:00 AM a 4:00 PM (Jornada continua).
-          </p>
-          <h5 class="mt-4 f-6">Contacto</h5>
-          <p class="m-0">
-            <b>Teléfono Conmutador:</b> (+57) 601 4391221 - WhatsApp +57 3124324495<br />
-            <b>Línea de atención Nacional:</b> 018000112021 (Solo teléfonos fijos fuera de Bogotá)<br />
-            <b>Correo Institucional:</b> <a href="mailto:correspondencia@fps.gov.co" target="_blank">correspondencia@fps.gov.co</a><br />
-            <b>Correo de Notificaciones judiciales:</b> <a href="mailto: notificacion.judicial@insor.gov.co" target="_blank"> notificacion.judicial@insor.gov.co</a><br />
-            <b>Correo anticorrupción:</b> <a href="mailto: : soytransparente@insor.gov.co" target="_blank"> soytransparente@insor.gov.co</a><br />
-          </p>
-        </div>
-        <div class="col-12 col-lg-5 d-none d-lg-block">
-          <div class="row justify-content-center">
-            <a href="/" rel="home" class="col-10 col-sm-8 col-md-7">
-              <img class="img-fluid" src="<?php bloginfo('template_url'); ?>/images/logos/logo-principal.png">
-            </a>
+            <h5 class="mt-4 f-6">Sede Principal</h5>
+            <p>
+              <b>Dirección: </b> Carrera 89A No. 64C–30. Álamos Zona Industrial. Bogotá D.C. – Colombia.<br />
+              <b>Código Postal:</b> 111071<br />
+              <b>Horario de atención Presencial:</b> Lunes a Viernes 8:00 AM a 4:00 PM (Jornada continua).
+            </p>
+            <h5 class="mt-4 f-6">Contacto</h5>
+            <p class="m-0">
+              <b>Teléfono Conmutador:</b> (+57) 601 4391221 - WhatsApp +57 3124324495<br />
+              <b>Línea de atención Nacional:</b> 018000112021 (Solo teléfonos fijos fuera de Bogotá)<br />
+              <b>Correo Institucional:</b> <a href="mailto:correspondencia@fps.gov.co" target="_blank">correspondencia@fps.gov.co</a><br />
+              <b>Correo de Notificaciones judiciales:</b> <a href="mailto: notificacion.judicial@insor.gov.co" target="_blank"> notificacion.judicial@insor.gov.co</a><br />
+              <b>Correo anticorrupción:</b> <a href="mailto: : soytransparente@insor.gov.co" target="_blank"> soytransparente@insor.gov.co</a><br />
+            </p>
           </div>
-          <div class="mt-4">
-            <div class="row redes">
-              <div class="col-12 col-lg-6">
-                <a
-                  href="https://twitter.com/fondo_pasivo"
-                  alt="Twitter"
-                  title="Ir a Twitter"
-                  target="_blank"
-                  class="icono-twitter">
-                  @insorcolombia
-                </a>
-              </div>
-              <div class="mt-2 mt-sm-0 col-12 col-lg-6">
-                <a
-                  href="https://www.instagram.com/fondo_pasivo/"
-                  alt="Instagram"
-                  title="Ir a Instagram"
-                  target="_blank"
-                  class="icono-instagram">
-                  @insorcolombiaoficial
-                </a>
-              </div>
-              <div class="mt-2 col-12 col-lg-6">
-                <a
-                  href="https://www.facebook.com/Fondo-de-Pasivo-Social-Ferrocarriles-Nacionales-de-Colombia-727728047616712/"
-                  alt="Facebook"
-                  title="Ir a Facebook"
-                  target="_blank"
-                  class="icono-facebook">
-                  InsorColombiaOficial
-                </a>
-              </div>
-              <div class="mt-2 col-12 col-lg-6">
-                <a
-                  href="https://www.youtube.com/channel/UC6hYgr708Glp23EiapxTH-Q"
-                  alt="Youtube"
-                  title="Ir a Youtube"
-                  target="_blank"
-                  class="icono-youtube">
-                  @INSOR_colombia
-                </a>
+          <div class="col-12 col-lg-5 d-none d-lg-block">
+            <div class="row justify-content-center">
+              <a href="/" rel="home" class="col-10 col-sm-8 col-md-7">
+                <img class="img-fluid" src="<?php bloginfo('template_url'); ?>/images/logos/logo-principal.png">
+              </a>
+            </div>
+            <div class="mt-4">
+              <div class="row redes">
+                <div class="col-12 col-lg-6">
+                  <a
+                    href="https://twitter.com/fondo_pasivo"
+                    alt="Twitter"
+                    title="Ir a Twitter"
+                    target="_blank"
+                    class="icono-twitter">
+                    @insorcolombia
+                  </a>
+                </div>
+                <div class="mt-2 mt-sm-0 col-12 col-lg-6">
+                  <a
+                    href="https://www.instagram.com/fondo_pasivo/"
+                    alt="Instagram"
+                    title="Ir a Instagram"
+                    target="_blank"
+                    class="icono-instagram">
+                    @insorcolombiaoficial
+                  </a>
+                </div>
+                <div class="mt-2 col-12 col-lg-6">
+                  <a
+                    href="https://www.facebook.com/Fondo-de-Pasivo-Social-Ferrocarriles-Nacionales-de-Colombia-727728047616712/"
+                    alt="Facebook"
+                    title="Ir a Facebook"
+                    target="_blank"
+                    class="icono-facebook">
+                    InsorColombiaOficial
+                  </a>
+                </div>
+                <div class="mt-2 col-12 col-lg-6">
+                  <a
+                    href="https://www.youtube.com/channel/UC6hYgr708Glp23EiapxTH-Q"
+                    alt="Youtube"
+                    title="Ir a Youtube"
+                    target="_blank"
+                    class="icono-youtube">
+                    @INSOR_colombia
+                  </a>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <div class="col-12 mt-4">
-          <div class="gov-link">
-            <div class="row">
-              <div class="col-12 col-lg-4">
-                <a class="" href="#" target="_blank">Política de privacidad y condiciones de uso</a>
-              </div>
-              <div class="col-12 col-lg-5">
-                <a class="" href="#" target="_blank">Política de privacidad y tratamiento de datos personales</a>
-              </div>
-              <div class="col-12 col-lg-1">
-                <a class="" href="#" target="_blank">Contacto</a>
-              </div>
-              <div class="col-12 col-lg-2">
-                <a class="" href="#" target="_blank">Mapa del sitio</a>
+          <div class="col-12 mt-4">
+            <div class="gov-link">
+              <div class="row">
+                <div class="col-12 col-lg-4">
+                  <a class="" href="#" target="_blank">Política de privacidad y condiciones de uso</a>
+                </div>
+                <div class="col-12 col-lg-5">
+                  <a class="" href="#" target="_blank">Política de privacidad y tratamiento de datos personales</a>
+                </div>
+                <div class="col-12 col-lg-1">
+                  <a class="" href="#" target="_blank">Contacto</a>
+                </div>
+                <div class="col-12 col-lg-2">
+                  <a class="" href="#" target="_blank">Mapa del sitio</a>
+                </div>
               </div>
             </div>
           </div>
